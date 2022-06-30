50d85a8197b7b3594ac4dce8f49bd42e63bd4a44
lebo.mofokeng <lebo.mofokeng@DDNAD4R7DB22>
2022-06-30 12:34:57 +0200

    Add project files.

---
+++ odd numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+
+namespace odd_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            for(int x = 5; x <= 500; x++)
+            {
+                if(x % 5 == 0)
+                {
+                    Write("{0,5}",x);   
+                    counter++;
+                    if(counter == 10)
+                    {
+                        Write("\n");
+                        counter = 0;
+                    }
+                }
+            }
+            ReadKey();
+        }
+    }
+}
